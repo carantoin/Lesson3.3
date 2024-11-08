bbfcea73688b123f323af967bbcc370ae11bbe4e
carantoin <90127329+carantoin@users.noreply.github.com>
2024-11-08 15:49:21 +0200

    Add files via upload

---
+++ Lesson3.3/Lesson3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿try
+{
+    Console.Write("Введите n:");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    int S = -1;
+    double s = 1;
+    for (int i = 3; i <= n; i++)
+    {
+        int coeff = 2 * i + 1;
+        double fact = (2 * i);
+        double term = S * (coeff / fact) * Math.Pow(x,2*i);
+        s += term;
+        S = -S;
+    }
+    Console.WriteLine($"Сумма {n} членов равна {s:F2}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
